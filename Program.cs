14fcb4b7ae2a48851e043eba792f90003f42edde
Aulganon <99094935+Aulganon@users.noreply.github.com>
2023-11-19 14:53:31 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graf2
+{
+    internal class Program
+    {
+        static public List<bool> SetArrayPos(List<bool> list, int number) {
+            List<bool> ret_list = list;
+            for (int i = 0; i < list.Count; ++i) {
+                if (number == i)
+                {
+                    ret_list[i] = true;
+                }
+                else {
+                    continue;
+                }
+                
+            }
+
+            return ret_list;
+        }
+
+
+        static bool Passed(List<bool> list) { 
+            //returns true if task is passed 
+            // false if not passed
+            // true by default must not be true at the end
+            bool ret = true;
+            foreach (bool b in list)
+            {
+                if (b == false) { 
+                    ret = false;
+                }
+            }
+            return ret;
+        }
+
+
+        static void SubMain(List<List<int>> Array)
+        {
+            Console.WriteLine("Select graph's vertex \nNote that position " +
+                "of 0 graph is restrincted");
+            int vertex_number = Int32.Parse(Console.ReadLine());
+
+            List<bool> Accesable = new List<bool>();
+            List<bool> Pass = new List<bool>();
+
+            //filling with falses
+            for (int i = 0; i < Array.Count; ++i) {
+                Accesable.Add(false);
+                Pass.Add(false);
+            }
+            
+
+
+            if (vertex_number > 0 && vertex_number <= Array.Count)
+            {
+                //set first vertex as passed
+                Accesable = SetArrayPos(Accesable, vertex_number - 1);
+                int while_counter = 0;
+                while (while_counter < Array.Count) {
+                    // if we haven't yet found a way to vertex
+                    if (Passed(Accesable) != true)
+                    {
+                        int counter = 0;
+                        foreach (var sublists in Array)
+                        {
+                            // if not passed and could be reached
+                            if (Pass[counter] == false && Accesable[counter] == true)
+                            {
+                                Pass = SetArrayPos(Pass, counter);
+                                int element_counter = 0;
+                                foreach (var element in sublists)
+                                {
+                                    if (element == 1)
+                                    {
+                                        Accesable = SetArrayPos(Accesable, element_counter);
+                                    }
+                                    element_counter++;
+                                }
+                            }
+                            counter++;
+                        }
+                    }
+                    else {
+                        break;
+                    }
+                    ++while_counter;
+                }
+                
+            }
+            // exceptions only 
+            else if (vertex_number > Array.Count)
+            {
+                Console.WriteLine("Are you OK? You require numbers that isn't stated in adjacency matrix." +
+                    "\n Change the number of vertex or add some more rows&collumns " +
+                    "into adjacency matrix.");
+            }
+            else
+            {
+                Console.WriteLine("Wrong vertex number. " +
+                    "Plaese try another");
+            }
+
+            int print_counter = 1;
+            Console.WriteLine(String.Format("--Vertex [{0}] is selected--\n", vertex_number));
+            int check = 0;
+            foreach (var elem in Accesable)
+            {
+                if (Pass[print_counter - 1] == true)
+                {
+                    ++check;
+                }
+                ++print_counter;
+            }
+            if (check == 1)
+            {
+                Pass[vertex_number-1] = true;
+            }
+
+            print_counter = 1;
+            foreach (var elem in Accesable) {
+                if (Pass[print_counter - 1] == false) {
+                    ++check;
+                    Console.WriteLine(
+                        String.Format("Vertex [{0}] is non-achivable", print_counter)
+                        );
+
+                }
+                
+                ++print_counter;
+            }
+            //достижимость писать тут
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            string path = Matrix.GetPath();
+            List<List<int>> Array = Matrix.ExtractFromFile(path);
+            SubMain(Array);
+            Console.WriteLine("\n\n\n\nPress ANY KEY to EXIT");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ matrix.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graf2
+{
+    internal class Matrix
+    {
+        static public string GetPath()
+        {
+            string path = "C:\\Users\\Alganon\\source\\repos\\graf1\\matrixs";
+
+            Console.WriteLine("Write subpaths");
+            string subpaths = Console.ReadLine();
+            List<string> line = subpaths
+                        .Split(new char[] { '\r', '\n', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                        .ToList();
+
+            foreach (string arg in line)
+            {
+                path += ("\\" + arg);
+            }
+            path += ".txt";
+            return path;
+        }
+
+        static public List<List<int>> ExtractFromFile(string path)
+        {
+            List<List<int>> TempArray = new List<List<int>>();
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Couldn't find the file at such derectory maybe you should try check file location?");
+            }
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                bool pass_f = true;
+                while (!sr.EndOfStream)
+                {
+                    if (pass_f)
+                    {
+                        sr.ReadLine();
+                        pass_f = false;
+                        continue;
+                    }
+                    List<int> line = sr.ReadLine()
+                        .Split(new char[] { '\r', '\n', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(s => Convert.ToInt32(s)).ToList();
+
+                    TempArray.Add(line);
+                }
+            }
+            return TempArray;
+        }
+
+        
+    }
+}
